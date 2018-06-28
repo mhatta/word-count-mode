8c41938e0148d55a1498f4a9562b860bc0f6e493
Masayuki Hatta <mhatta@debian.org>
2018-06-28 15:30:55 +0900

    Updated.

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # word-count-mode
-Hiroyuki Komatsu word-count-mode updated to Emacs24
 
-Orignally found at http://taiyaki.org/elisp/word-count/src/word-count.el (down on 2012-11-01)
+This minor mode shows the number of words/characters in the current buffer in the mode line.
 
-Wayback Machine link working 2015-07-16: http://web.archive.org/web/20100924082154/http://taiyaki.org/elisp/word-count/src/word-count.el
+# Usage
+
+Put something like this in init.el:
+
+``` emacs-lisp
+(autoload 'word-count-mode "word-count"
+	"Minor mode to count words." t nil)
+(global-set-key "\M-+" 'word-count-mode)
+```
+
+Then
+
+* M-+ (word-count-mode) toggles word-count mode.
+* M-[space] (word-count-set-area) sets area for counting words.
+* M-x word-count-set-region sets region or paragraph for counting words.
+* M-x word-count-set-marker sets marker for counting words.
+
+# Author and Maintainers
+
+This emacs lisp was originally written by Hiroyuki Komatsu (@hiroyuki-komatsu) years ago.  The upstream site has long gone, but still you can see the original version using Wayback Machine: [http://web.archive.org/web/20100924082154/http://taiyaki.org/elisp/word-count/src/word-count.el](http://web.archive.org/web/20100924082154/http://taiyaki.org/elisp/word-count/src/word-count.el).
+
+Tomasz Skutnik (@tomaszskutnik) salvaged and updated word-count-mode for Emacs24.  Now Masayuki Hatta (@mhatta) updated it for Emacs26 and prepared for MELPA.
+
+# License
+
+ Licensed under the same terms as Emacs.
--- word-count.el
+++ word-count.el
@@ -1,63 +1,66 @@
-;;;;; word-count.el - Counting word for Emacsen.
-;;
-;; AUTHOR:  Hiroyuki Komatsu <komatsu@taiyaki.org>
-;; LICENCE: GPL2
-;; $Id: word-count.el,v 1.3 2003/10/09 22:40:40 komatsu Exp $
-;;
-;; How to install:
-;; 1). Download this file from the following URL.
-;; <http://www.taiyaki.org/elisp/word-count/src/wourd-count.el>
-;;
-;; 2). Where this file is stored to ~/elisp/, please add the following lines to your ".emacs".
-;; 
-;; (setq load-path (cons (expand-file-name "~/elisp") load-path))
+;;; word-count.el --- show the number of words in the mode line. -*- lexical-binging: t -*-
+
+;; Copyright (C) 2012-2018 Hiroyuki Komatsu
+
+;; Author:  Hiroyuki Komatsu
+;; Maintainer: Tomasz Skutnik and Masayuki Hatta
+
+;; URL: https://github.com/mhatta/word-count-mode
+;; Version: 1
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; Licensed under the same terms as Emacs.
+
+;;; Commentary:
+
+;; 1) M-+ (word-count-mode) toggles word-count mode.
+;; 2) M-[space] (word-count-set-area) sets area for counting words.
+;; 3) M-x word-count-set-region sets region or paragraph for counting words.
+;; 4) M-x word-count-set-marker sets marker for counting words.
+
+;; Put the following in init.el.
+
 ;; (autoload 'word-count-mode "word-count"
 ;;           "Minor mode to count words." t nil)
 ;; (global-set-key "\M-+" 'word-count-mode)
-;;
-;; How to use:
-;; 1). M-+ (word-count-mode) toggles word-count mode.
-;; 2). M-[space] (word-count-set-area) sets area for counting words.
-;; 3). M-x word-count-set-region sets region or paragraph for counting words.
-;; 4). M-x word-count-set-marker sets marker for counting words.
 
-
-;; ----------------------------------------------------------------------
-;; Mell
-;; ----------------------------------------------------------------------
+;;; Code:
 
 ;; Checking Emacs or XEmacs. (APEL?)
 (if (not (boundp 'running-xemacs))
-    (defconst running-xemacs nil))
+    (defconst word-count-running-xemacs nil))
 
-(defun mell-check-value (value)
+(defun word-count-check-value (value)
   (and (boundp value)
        (symbol-value value)))
 
-;; mell-region-active-p
-(if running-xemacs
-    (defun mell-region-active-p ()
+;; word-count-region-active-p
+(if word-count-running-xemacs
+    (defun word-count-region-active-p ()
       (region-active-p))
-  (defun mell-region-active-p ()
-    (mell-check-value 'mark-active))
+  (defun word-count-region-active-p ()
+    (word-count-check-value 'mark-active))
   )
 
-;; mell-transient-mode-p
-(if running-xemacs
-    (defun mell-transient-mode-p ()
-      (mell-check-value 'zmacs-regions))
-  (defun mell-transient-mode-p ()
-    (mell-check-value 'transient-mark-mode))
+;; word-count-transient-mode-p
+(if word-count-running-xemacs
+    (defun word-count-transient-mode-p ()
+      (word-count-check-value 'zmacs-regions))
+  (defun word-count-transient-mode-p ()
+    (word-count-check-value 'transient-mark-mode))
   )
 
-;; Define mell-transient-region-active-p
-(defun mell-transient-region-active-p ()
-  (and (mell-transient-mode-p)
-       (mell-region-active-p)))
+;; Define word-count-transient-region-active-p
+(defun word-count-transient-region-active-p ()
+  (and (word-count-transient-mode-p)
+       (word-count-region-active-p)))
 
 ;; ------------------------------------------------------------
 
-(defun mell-marker-set (marker &optional position buffer type)
+(defun word-count-marker-set (marker &optional position buffer type)
   (or (markerp (eval marker))
       (set marker (make-marker)))
   (or position
@@ -66,45 +69,45 @@
   (set-marker-insertion-type (eval marker) type)
   )
 
-(defun mell-defvar (symbol value &optional doc-string)
+(defun word-count-defvar (symbol value &optional doc-string)
   (if (not (boundp symbol))
       (set symbol value))
   (if doc-string
       (put symbol 'variable-documentation doc-string))
   symbol)
 
-(defun mell-defvar-locally (symbol initvalue &optional docstring)
-  (mell-defvar symbol initvalue docstring)
+(defun word-count-defvar-locally (symbol initvalue &optional docstring)
+  (word-count-defvar symbol initvalue docstring)
   (make-variable-buffer-local symbol)
   symbol)
 
-(defun mell-set-minor-mode (name modeline &optional key-map)
+(defun word-count-set-minor-mode (name modeline &optional key-map)
   (make-variable-buffer-local name)
   (setq minor-mode-alist
-	(mell-alist-add minor-mode-alist (list name modeline)))
+	(word-count-alist-add minor-mode-alist (list name modeline)))
   (and key-map
        (setq minor-mode-map-alist
-	     (mell-alist-add minor-mode-map-alist (cons name key-map)))
+	     (word-count-alist-add minor-mode-map-alist (cons name key-map)))
        )
   )
 
-(defun mell-point-at-bop (&optional point)
+(defun word-count-point-at-bop (&optional point)
   (save-excursion
     (goto-char (or point (point)))
     (backward-paragraph 1)
     (point)))
 
-(defun mell-point-at-eop (&optional point)
+(defun word-count-point-at-eop (&optional point)
   (save-excursion
     (goto-char (or point (point)))
     (forward-paragraph 1)
     (point)))
 
-;; mell-alist ------------------------------------------------------------
+;; word-count-alist ------------------------------------------------------------
 
-(defun mell-alist-add! (alist new-cons)
+(defun word-count-alist-add! (alist new-cons)
   (if (null alist)
-      (error "mell-alist-add! can not deal nil as an alist.")
+      (error "word-count-alist-add! can not deal nil as an alist.")
     (let ((current-cons (assoc (car new-cons) alist)))
       (if current-cons
 	  (setcdr current-cons (cdr new-cons))
@@ -114,17 +117,17 @@
 	)
       alist)))
   
-(defun mell-alist-add (alist new-cons)
+(defun word-count-alist-add (alist new-cons)
   (if (null alist)
       (list new-cons)
     (let ((return-alist (copy-alist alist)))
-      (mell-alist-add! return-alist new-cons)
+      (word-count-alist-add! return-alist new-cons)
       return-alist)))
   
-(defun mell-alist-delete (alist key)
+(defun word-count-alist-delete (alist key)
   (if key
       (let (return-alist)
-	(mapcar '(lambda (x)
+	(mapcar #'(lambda (x)
 		   (or (equal key (car x))
 		       (setq return-alist (cons x return-alist))))
 		alist)
@@ -134,7 +137,7 @@
     alist)
   )
 
-(defun mell-alist-get-value (key alist)
+(defun word-count-alist-get-value (key alist)
   "Return a value corresponded to KEY or 't' from ALIST."
   (if (consp alist)
       (let ((assoc-pair (assoc key alist)))
@@ -143,9 +146,9 @@
           (cdr (assoc t alist))))
     alist))
 
-;; mell-string ------------------------------------------------------------
+;; word-count-string ------------------------------------------------------------
 
-(defun mell-string-split (string regexp)
+(defun word-count-string-split (string regexp)
   "Divide STRING from REGEXP."
   (let ((start 0) match-list splited-list)
     (while (string-match regexp string start)
@@ -162,19 +165,19 @@
       )
     (reverse splited-list)))
 
-(defun mell-string-replace (target-string from-regexp to-string)
+(defun word-count-string-replace (target-string from-regexp to-string)
   "Replace TARGET-STRING from FROM-REGEXP to TO-STRING."
   (if (string-match from-regexp target-string)
       (setq target-string
-	    (mapconcat '(lambda (x) x)
-		       (mell-string-split target-string from-regexp)
+	    (mapconcat #'(lambda (x) x)
+		       (word-count-string-split target-string from-regexp)
 		       to-string))
     )
   target-string)
 
-;; mell-match ------------------------------------------------------------
+;; word-count-match ------------------------------------------------------------
 
-(defun mell-match-count-string (regexp string)
+(defun word-count-match-count-string (regexp string)
   (save-match-data
     (let ((i 0) (n 0))
       (while (and (string-match regexp string i) (< i (match-end 0)))
@@ -182,79 +185,79 @@
 	(setq n (1+ n)))
       n)))
   
-(if running-xemacs
+(if word-count-running-xemacs
     (eval
-     '(defun mell-match-count-region (regexp start end &optional buffer)
-	(mell-match-count-string regexp (buffer-substring start end buffer))
+     '(defun word-count-match-count-region (regexp start end &optional buffer)
+	(word-count-match-count-string regexp (buffer-substring start end buffer))
 	))
   (eval
-   '(defun mell-match-count-region (regexp start end &optional buffer)
+   '(defun word-count-match-count-region (regexp start end &optional buffer)
       (save-excursion
 	(and buffer (set-buffer buffer))
-	(mell-match-count-string regexp (buffer-substring start end))
+	(word-count-match-count-string regexp (buffer-substring start end))
 	)))
   )
 
-;; mell-sign ------------------------------------------------------------
+;; word-count-sign ------------------------------------------------------------
 
-(defun mell-color-find (color-name &optional alt-tty-color-num)
+(defun word-count-color-find (color-name &optional alt-tty-color-num)
   (if window-system color-name
     (and (functionp 'find-tty-color)
 	 (or (and color-name (find-tty-color color-name))
 	     (nth alt-tty-color-num (tty-color-list))))
     ))
 
-(defvar mell-sign-marker-overlay-alist (list nil))
-(defun mell-sign-marker (marker &optional face)
-  (let ((overlay (cdr (assoc marker mell-sign-marker-overlay-alist)))
+(defvar word-count-sign-marker-overlay-alist (list nil))
+(defun word-count-sign-marker (marker &optional face)
+  (let ((overlay (cdr (assoc marker word-count-sign-marker-overlay-alist)))
 	(start (min marker (1- (point-max)))) ;; for EOB
 	(end (min (1+ marker) (point-max))))
     (if overlay
 	(move-overlay overlay start end (marker-buffer marker))
       (setq overlay (make-overlay start end (marker-buffer marker)))
-      (mell-alist-add! mell-sign-marker-overlay-alist (cons marker overlay))
+      (word-count-alist-add! word-count-sign-marker-overlay-alist (cons marker overlay))
       )
     (overlay-put overlay 'face (or face 'highlight))
     (overlay-put overlay 'evaporate t)
-    (add-hook 'post-command-hook 'mell-sign-marker-redisplay t t)
+    (add-hook 'post-command-hook 'word-count-sign-marker-redisplay t t)
     ))
 
-(defun mell-sign-marker-off (marker)
-  (let ((overlay (cdr (assoc marker mell-sign-marker-overlay-alist))))
+(defun word-count-sign-marker-off (marker)
+  (let ((overlay (cdr (assoc marker word-count-sign-marker-overlay-alist))))
     (if overlay
 	(delete-overlay overlay))
-    (setq mell-sign-marker-overlay-alist
-	  (mell-alist-delete mell-sign-marker-overlay-alist marker))
-    (remove-hook 'post-command-hook 'mell-sign-marker-redisplay t)
+    (setq word-count-sign-marker-overlay-alist
+	  (word-count-alist-delete word-count-sign-marker-overlay-alist marker))
+    (remove-hook 'post-command-hook 'word-count-sign-marker-redisplay t)
     ))
 
-(defun mell-sign-marker-redisplay ()
+(defun word-count-sign-marker-redisplay ()
   (mapcar 
-   '(lambda (cons) (mell-sign-marker (car cons)))
-   mell-sign-marker-overlay-alist))
+   #'(lambda (cons) (word-count-sign-marker (car cons)))
+   word-count-sign-marker-overlay-alist))
 
-(defvar mell-sign-region-overlay-alist (list nil))
-(defun mell-sign-region (start end &optional buffer face)
+(defvar word-count-sign-region-overlay-alist (list nil))
+(defun word-count-sign-region (start end &optional buffer face)
   (or buffer (setq buffer (current-buffer)))
   (let* ((region (list start end buffer))
-	 (overlay (cdr (assoc region mell-sign-region-overlay-alist))))
+	 (overlay (cdr (assoc region word-count-sign-region-overlay-alist))))
     (if overlay
 	(move-overlay overlay start end buffer)
       (setq overlay (make-overlay start end buffer nil t))
-      (mell-alist-add! mell-sign-region-overlay-alist (cons region overlay))
+      (word-count-alist-add! word-count-sign-region-overlay-alist (cons region overlay))
       )
     (overlay-put overlay 'face (or face 'highlight))
     (overlay-put overlay 'evaporate t)
     ))
 
-(defun mell-sign-region-off (start end &optional buffer)
+(defun word-count-sign-region-off (start end &optional buffer)
   (or buffer (setq buffer (current-buffer)))
   (let* ((region (list start end buffer))
-	 (overlay (cdr (assoc region mell-sign-region-overlay-alist))))
+	 (overlay (cdr (assoc region word-count-sign-region-overlay-alist))))
     (if overlay
 	(delete-overlay overlay))
-    (setq mell-sign-region-overlay-alist
-	  (mell-alist-delete mell-sign-region-overlay-alist region))
+    (setq word-count-sign-region-overlay-alist
+	  (word-count-alist-delete word-count-sign-region-overlay-alist region))
     ))
 
 
@@ -282,13 +285,13 @@ A pair with 't' is a default.")
 (defcustom word-count-mode-init-hook nil
   "Function or functions called when word-count.el is loaded.")
 
-(defcustom word-count-marker-foreground (mell-color-find "#D0D0D0" 7)
+(defcustom word-count-marker-foreground (word-count-color-find "#D0D0D0" 7)
   "Color for word-count mode.")
-(defcustom word-count-marker-background (mell-color-find "#5050A0" 3)
+(defcustom word-count-marker-background (word-count-color-find "#5050A0" 3)
   "Color for word-count mode.")
-(defcustom word-count-region-foreground (mell-color-find "#D0D0D0" 7)
+(defcustom word-count-region-foreground (word-count-color-find "#D0D0D0" 7)
   "Color for word-count mode.")
-(defcustom word-count-region-background (mell-color-find "#5050A0" 3)
+(defcustom word-count-region-background (word-count-color-find "#5050A0" 3)
   "Color for word-count mode.")
 
 (if (not (boundp 'word-count-marker-face))
@@ -313,11 +316,11 @@ A pair with 't' is a default.")
 
 
 (defvar word-count-mode nil "*Non-nil means in an word-count mode.")
-(mell-set-minor-mode 'word-count-mode 'word-count-modeline word-count-mode-map)
+(word-count-set-minor-mode 'word-count-mode 'word-count-modeline word-count-mode-map)
 
-(mell-defvar-locally 'word-count-modeline " WC")
-(mell-defvar-locally 'word-count-marker-beginning nil)
-(mell-defvar-locally 'word-count-marker-end nil)
+(word-count-defvar-locally 'word-count-modeline " WC")
+(word-count-defvar-locally 'word-count-marker-beginning nil)
+(word-count-defvar-locally 'word-count-marker-end nil)
 
 (defun word-count-mode (&optional arg)
   (interactive "P")
@@ -332,7 +335,7 @@ A pair with 't' is a default.")
 (defun word-count-mode-on ()
   (interactive)
   (setq word-count-mode t)
-  (if (mell-transient-region-active-p)
+  (if (word-count-transient-region-active-p)
       (word-count-set-region)
     (word-count-set-marker))
   (add-hook 'post-command-hook 'word-count-modeline-display t t)
@@ -350,7 +353,7 @@ A pair with 't' is a default.")
   (interactive)
   (or word-count-mode
       (word-count-mode))
-  (if (mell-transient-region-active-p)
+  (if (word-count-transient-region-active-p)
       (word-count-set-region)
     (word-count-set-marker)
     ))
@@ -359,32 +362,32 @@ A pair with 't' is a default.")
   (interactive)
   (or word-count-mode (word-count-mode))
   (word-count-set-region-off)
-  (mell-marker-set 'word-count-marker-beginning)
-  (mell-sign-marker word-count-marker-beginning word-count-marker-face)
+  (word-count-marker-set 'word-count-marker-beginning)
+  (word-count-sign-marker word-count-marker-beginning word-count-marker-face)
   )
 
 (defun word-count-set-marker-off ()
-  (mell-sign-marker-off word-count-marker-beginning)
+  (word-count-sign-marker-off word-count-marker-beginning)
   )
 
 (defun word-count-set-region ()
   (interactive)
   (or word-count-mode (word-count-mode))
   (word-count-set-marker-off)
-  (if (mell-transient-region-active-p)
+  (if (word-count-transient-region-active-p)
       (progn
-	(mell-marker-set 'word-count-marker-beginning (min (mark) (point)))
-	(mell-marker-set 'word-count-marker-end (max (mark) (point)) nil t)
+	(word-count-marker-set 'word-count-marker-beginning (min (mark) (point)))
+	(word-count-marker-set 'word-count-marker-end (max (mark) (point)) nil t)
 	)
-    (mell-marker-set 'word-count-marker-beginning (mell-point-at-bop))
-    (mell-marker-set 'word-count-marker-end       (mell-point-at-eop) nil t)
+    (word-count-marker-set 'word-count-marker-beginning (word-count-point-at-bop))
+    (word-count-marker-set 'word-count-marker-end       (word-count-point-at-eop) nil t)
     )
-  (mell-sign-region word-count-marker-beginning word-count-marker-end nil
+  (word-count-sign-region word-count-marker-beginning word-count-marker-end nil
 		    word-count-region-face)
   )
 
 (defun word-count-set-region-off ()
-  (mell-sign-region-off word-count-marker-beginning word-count-marker-end)
+  (word-count-sign-region-off word-count-marker-beginning word-count-marker-end)
   (and (markerp word-count-marker-end)
        (set-marker word-count-marker-end nil))
   (setq word-count-marker-end nil)
@@ -401,7 +404,7 @@ A pair with 't' is a default.")
     (concat
      word-count-modeline-string
      (apply 'format "%d/%d/%d" (word-count-CWL-region beginning end))
-     (if (mell-transient-region-active-p)
+     (if (word-count-transient-region-active-p)
 	 (apply 'format " (%d/%d/%d)" (word-count-CWL-region)))
      )))
 
@@ -435,31 +438,36 @@ A pair with 't' is a default.")
   (or nopreremove
       (setq string (word-count-preremove-string string)))
   (- (length string)
-     (mell-match-count-string word-count-non-character-regexp string)
+     (word-count-match-count-string word-count-non-character-regexp string)
      ))
 
 (defun word-count-words-string (string &optional nopreremove)
   (or nopreremove
       (setq string (word-count-preremove-string string)))
-  (mell-match-count-string word-count-word-regexp string))
+  (word-count-match-count-string word-count-word-regexp string))
 
 (defun word-count-lines-string (string &optional nopreremove)
   (or nopreremove
       (setq string (word-count-preremove-string string)))
-  (- (1+ (mell-match-count-string
+  (- (1+ (word-count-match-count-string
 	  "\n" (substring string 0 (max 0 (1- (length string))))))
-     (mell-match-count-string word-count-non-line-regexp string)
+     (word-count-match-count-string word-count-non-line-regexp string)
      ))
 
 
 (defun word-count-preremove-string (string &optional patterns)
-  (mapcar '(lambda (pattern)
-	     (setq string (mell-string-replace string pattern " ")))
+  (mapcar #'(lambda (pattern)
+	     (setq string (word-count-string-replace string pattern " ")))
 	  (or patterns
-	      (mell-alist-get-value major-mode
+	      (word-count-alist-get-value major-mode
 				    word-count-preremove-regexp-alist)))
   string)
 
 (run-hooks 'word-count-mode-init-hook)
 (provide 'word-count)
 ;; ----------------------------------------------------------------------
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+;;; word-count.el ends here
